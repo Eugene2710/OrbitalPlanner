5453b53feba6d9bc96376efdb9d518e97a041572
liewfuteck <liewfuteck@gmail.com>
2018-06-22 17:32:16 +0800

    Enabling login function in main activity page

--- app/src/main/java/com/example/futeck/planner/MainActivity.java
+++ app/src/main/java/com/example/futeck/planner/MainActivity.java
@@ -1,22 +1,75 @@
 package com.example.futeck.planner;
 
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
+    private Button loginButton;
     private Button toRegisterPageButton;
+    private EditText emailField;
+    private EditText passwordField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton = findViewById(R.id.loginButt);
+        emailField = findViewById(R.id.emailusernameEditText);
+        passwordField = findViewById(R.id.passwordEditText);
         toRegisterPageButton = findViewById(R.id.RegisterButton);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                mUser = firebaseAuth.getCurrentUser();
+
+                if (mUser != null) {
+                    Toast.makeText(MainActivity.this, "Signed In", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Unable to sign in", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(emailField.getText().toString()) && (!TextUtils.isEmpty(passwordField.getText().toString()))) {
+                    String email = emailField.getText().toString();
+                    String pwd = passwordField.getText().toString();
+
+                    login(email, pwd);
+
+                } else {
+
+                }
+            }
+        });
+
 
         toRegisterPageButton.setOnClickListener(new View.OnClickListener() {
 
@@ -25,7 +78,29 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, Register.class);
                 startActivity(intent);
             }
-
         });
     }
+
+    private void login(String email, String pwd) {
+
+        mAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Unable to sign in", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 }
+
